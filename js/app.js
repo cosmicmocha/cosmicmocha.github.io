4ca28c429666ec2c54d50be0512088ef187b6a33
Valery <valerymarier@cosmicmocha.com>
2018-06-28 00:06:41 -0400

    updoot summer 2018

---
+++ js/app.js
@@ -0,0 +1,44 @@
+    var modal = document.querySelector(".modal");
+    var trigger = document.querySelector(".trigger");
+var trigger2 = document.querySelector(".trigger2");
+    var closeButton = document.querySelector(".close-button");
+
+
+    $(document).ready(function(){
+      $('.spin0').on('hover', function(){
+    $('#spin-in0').toggleClass('show0');
+});$('.spin1').on('hover', function(){
+$('#spin-in1').toggleClass('show1');
+});$('.spin2').on('hover', function(){
+$('#spin-in2').toggleClass('show2');
+});$('.spin3').on('hover', function(){
+$('#spin-in3').toggleClass('show3');
+});
+    //super sassy back to top button
+
+    var windowHeight = $(window).height();
+    $(document).scroll(function() {
+    $('#to-top').toggle($(this).scrollTop() > windowHeight-300);
+    });
+    });
+
+    function toggleModal() {
+        modal.classList.toggle("show-modal");
+        $("#modalfade").fadeToggle(750);
+        $(".socials").fadeToggle(750);
+        $(".intro").fadeToggle(750);
+        $(".slug").fadeToggle(750);
+        $(".portfolio-btn").fadeToggle(750);
+    }
+
+    function windowOnClick(event) {
+        if (event.target === modal) {
+            toggleModal();
+        }
+    }
+
+
+    trigger.addEventListener("click", toggleModal);
+ trigger2.addEventListener("click", toggleModal);
+    closeButton.addEventListener("click", toggleModal);
+    window.addEventListener("click", windowOnClick);
