6852f8bb9254247e9d851612f615ca4d615430fc
Valery Marier <valery@Valerys-MacBook-Pro.local>
2019-10-07 22:21:36 -0400

    RGD Critiques
    Updated based on crits from various RGD members

--- js/app_index.js
+++ js/app_index.js
@@ -11,30 +11,81 @@ $("a").on('click', function(event) { // when an anchor is clicked.
     }
 });
 
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("#scroll-icon").style.color = "#000";
+  } else {
+    document.getElementById("scroll-icon").style.color = "#fff";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
 $(".hover-about").hover(function() { // Mouse over
   $(".hover-about").addClass('highlight-about');
   $( ".frame" ).addClass('frame-about');
+  $( ".frame" ).removeClass('frame-home');
   $(".index-active").addClass('quiet-links');
+  $(".index-active").removeClass('highlight-home');
   $(".hover-work").addClass('quiet-links')
   $(".hover-play").addClass('quiet-links')
 }, function() { // Mouse out
       $(".hover-about").removeClass('highlight-about');
+      $( ".frame" ).addClass('frame-home');
       $( ".frame" ).removeClass('frame-about');
+      $(".index-active").addClass('highlight-home');
       $(".index-active").removeClass('quiet-links');
       $(".hover-work").removeClass('quiet-links')
       $(".hover-play").removeClass('quiet-links')
+
+});
+$(".hover-index-about").hover(function() { // Mouse over
+  $( ".frame" ).addClass('frame-about');
+  $( ".frame" ).removeClass('frame-home');
+  $(".index-active").addClass('quiet-links');
+  $(".index-active").removeClass('highlight-home');
+  $(".hover-work").addClass('quiet-links')
+  $(".hover-play").addClass('quiet-links')
+}, function() { // Mouse out
+      $( ".frame" ).addClass('frame-home');
+      $( ".frame" ).removeClass('frame-about');
+      $(".index-active").addClass('highlight-home');
+      $(".index-active").removeClass('quiet-links');
+      $(".hover-work").removeClass('quiet-links')
+      $(".hover-play").removeClass('quiet-links')
+
 });
 
 $(".hover-work").hover(function() { // Mouse over
   $(".hover-work").addClass('highlight-work');
   $( ".frame" ).addClass('frame-work');
+  $( ".frame" ).removeClass('frame-home');
   $(".index-active").addClass('quiet-links');
+  $(".index-active").removeClass('highlight-home');
   $(".hover-about").addClass('quiet-links');
   $(".hover-play").addClass('quiet-links')
 }, function() { // Mouse out
   $(".hover-work").removeClass('highlight-work');
+  $( ".frame" ).addClass('frame-home');
   $( ".frame" ).removeClass('frame-work');
   $(".index-active").removeClass('quiet-links');
+  $(".index-active").addClass('highlight-home');
+  $(".hover-about").removeClass('quiet-links');
+  $(".hover-play").removeClass('quiet-links')
+});
+$(".hover-index-work").hover(function() { // Mouse over
+  $( ".frame" ).addClass('frame-work');
+  $( ".frame" ).removeClass('frame-home');
+  $(".index-active").addClass('quiet-links');
+  $(".index-active").removeClass('highlight-home');
+  $(".hover-about").addClass('quiet-links');
+  $(".hover-play").addClass('quiet-links')
+}, function() { // Mouse out
+  $( ".frame" ).addClass('frame-home');
+  $( ".frame" ).removeClass('frame-work');
+  $(".index-active").removeClass('quiet-links');
+  $(".index-active").addClass('highlight-home');
   $(".hover-about").removeClass('quiet-links');
   $(".hover-play").removeClass('quiet-links')
 });
@@ -42,15 +93,19 @@ $(".hover-work").hover(function() { // Mouse over
 $(".hover-play").hover(function() { // Mouse over
   $(".hover-play").addClass('highlight-play');
   $( ".frame" ).addClass('frame-play');
+  $( ".frame" ).removeClass('frame-home');
   $(".index-active").addClass('quiet-links');
+  $(".index-active").removeClass('highlight-home');
   $(".hover-about").addClass('quiet-links');
   $(".hover-work").addClass('quiet-links');
 
 }, function() { // Mouse out
   $(".hover-play").removeClass('highlight-play');
+  $( ".frame" ).addClass('frame-home');
       $( ".frame" ).removeClass('frame-play');
       $(".hover-about").removeClass('quiet-links');
       $(".index-active").removeClass('quiet-links');
+      $(".index-active").addClass('highlight-home');
       $(".hover-work").removeClass('quiet-links');
 
 });
