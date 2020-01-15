bb7f05837c3657704d8a43781a0fc76b051272b6
Valery Marier <valery@Valerys-MacBook-Pro.local>
2020-01-15 12:52:21 -0500

    2020-01-15
    - Fluid Typesetting
    - Added Come Projects
    - Tidied up CSS

---
+++ js/typesetting.js
@@ -0,0 +1,123 @@
+Textblock([
+  {
+    target: "BODY",
+    minWidth: 400,
+    maxWidth: 1440,
+    minWidth_FontSize: 2.0,
+    maxWidth_FontSize: 2.0,
+    minWidth_LineHeight: 1,
+    maxWidth_LineHeight: 1,
+    container: "grid",
+    units: "em"
+  },
+  {
+    target: "NAV",
+    minWidth: 400,
+    maxWidth: 1440,
+    minWidth_FontSize: 1.6,
+    maxWidth_FontSize: 1.8,
+    minWidth_LineHeight: 1,
+    maxWidth_LineHeight: 1,
+    container: "grid",
+    units: "rem"
+  },
+  {
+    target: "H1",
+    minWidth: 400,
+    maxWidth: 1440,
+    minWidth_FontSize: 2.0,
+    maxWidth_FontSize: 2.7,
+    minWidth_LineHeight: 2,
+    maxWidth_LineHeight: 2,
+    container: "grid",
+    units: "rem"
+  },
+  {
+    target: "BLOCKQUOTE, H2",
+    minWidth: 400,
+    maxWidth: 1440,
+    minWidth_FontSize: 1.8,
+    maxWidth_FontSize: 2.5,
+    minWidth_LineHeight: 2,
+    maxWidth_LineHeight: 2,
+    container: "grid",
+    units: "rem"
+  },
+  {
+    target: "H3",
+    minWidth: 400,
+    maxWidth: 1440,
+    minWidth_FontSize: 1.6,
+    maxWidth_FontSize: 2.0,
+    minWidth_LineHeight: 1.6,
+    maxWidth_LineHeight: 1.6,
+    container: "grid",
+    units: "rem"
+  },
+  {
+    target: "MAIN P",
+    minWidth: 400,
+    maxWidth: 1440,
+    minWidth_FontSize: 1.6,
+    maxWidth_FontSize: 2.0,
+    minWidth_LineHeight: 1.6,
+    maxWidth_LineHeight: 1.6,
+    container: "grid",
+    units: "rem"
+  },
+  {
+    target: "MAIN P EM, MAIN P A",
+    minWidth: 400,
+    maxWidth: 1440,
+    minWidth_FontSize: 1.6,
+    maxWidth_FontSize: 2.0,
+    minWidth_LineHeight: 1.6,
+    maxWidth_LineHeight: 1.6,
+    container: "grid",
+    units: "rem"
+  },
+  {
+    target: ".index-desc",
+    minWidth: 400,
+    maxWidth: 1440,
+    minWidth_FontSize: 1.8,
+    maxWidth_FontSize: 2.5,
+    minWidth_LineHeight: 1.6,
+    maxWidth_LineHeight: 1.6,
+    container: "grid",
+    units: "rem"
+  },
+  {
+    target: "H4",
+    minWidth: 400,
+    maxWidth: 1440,
+    minWidth_FontSize: 1.6,
+    maxWidth_FontSize: 2.0,
+    minWidth_LineHeight: 1.0,
+    maxWidth_LineHeight: 1.0,
+    container: "grid",
+    units: "rem"
+  },
+  {
+    target: "FOOTER",
+    minWidth: 400,
+    maxWidth: 1440,
+    minWidth_FontSize: 1.4,
+    maxWidth_FontSize: 1.8,
+    minWidth_LineHeight: 1.8,
+    maxWidth_LineHeight: 1.8,
+    container: "grid",
+    units: "rem"
+  },
+  {
+    target: "FOOTER A",
+    minWidth: 400,
+    maxWidth: 1440,
+    minWidth_FontSize: 1.4,
+    maxWidth_FontSize: 1.8,
+    minWidth_LineHeight: 1.8,
+    maxWidth_LineHeight: 1.8,
+    container: "grid",
+    units: "rem"
+  },
+]);
---
+++ work/live/michelle/js/typesetting.js
@@ -0,0 +1,123 @@
+Textblock([
+  {
+    target: "H1",
+    minWidth: 400,
+    maxWidth: 1440,
+    minWidth_FontSize: 92,
+    maxWidth_FontSize: 114,
+    minWidth_LineHeight: 1,
+    maxWidth_LineHeight: 1,
+    container: "grid",
+    units: "px"
+  },
+  {
+    target: "H2",
+    minWidth: 400,
+    maxWidth: 1440,
+    minWidth_FontSize: 18,
+    maxWidth_FontSize: 72,
+    minWidth_LineHeight: 1,
+    maxWidth_LineHeight: 1,
+    container: "grid",
+    units: "px"
+  },
+  {
+    target: "HEADER H2",
+    minWidth: 400,
+    maxWidth: 1440,
+    minWidth_FontSize: 18,
+    maxWidth_FontSize: 30,
+    minWidth_LineHeight: 1.5,
+    maxWidth_LineHeight: 1.5,
+    container: "grid",
+    units: "px"
+  },
+  {
+    target: "H3",
+    minWidth: 400,
+    maxWidth: 1440,
+    minWidth_FontSize: 18,
+    maxWidth_FontSize: 42,
+    minWidth_LineHeight: 2,
+    maxWidth_LineHeight: 2,
+    container: "grid",
+    units: "px"
+  },
+  {
+    target: "H4",
+    minWidth: 400,
+    maxWidth: 1440,
+    minWidth_FontSize: 18,
+    maxWidth_FontSize: 46,
+    minWidth_LineHeight: 1.25,
+    maxWidth_LineHeight: 1.25,
+    container: "grid",
+    units: "px"
+  },
+  {
+    target: "H6",
+    minWidth: 400,
+    maxWidth: 1440,
+    minWidth_FontSize: 63,
+    maxWidth_FontSize: 700,
+    minWidth_LineHeight: 0.5,
+    maxWidth_LineHeight: 0.5,
+    container: "grid",
+    units: "px"
+  },
+  {
+    target: "H5",
+    minWidth: 400,
+    maxWidth: 1440,
+    minWidth_FontSize: 63,
+    maxWidth_FontSize: 400,
+    minWidth_LineHeight: 1,
+    maxWidth_LineHeight: 1,
+    container: "grid",
+    units: "px"
+  },
+  {
+    target: ".nav, .footer, P A",
+    minWidth: 400,
+    maxWidth: 1440,
+    minWidth_FontSize: 14,
+    maxWidth_FontSize: 16,
+    minWidth_LineHeight: 1.5,
+    maxWidth_LineHeight: 1.5,
+    container: "grid",
+    units: "px"
+  },
+  {
+    target: "P",
+    minWidth: 400,
+    maxWidth: 1440,
+    minWidth_FontSize: 16,
+    maxWidth_FontSize: 18,
+    minWidth_LineHeight: 1.5,
+    maxWidth_LineHeight: 1.5,
+    container: "grid",
+    units: "px"
+  },
+  {
+    target: ".specimen-11 P",
+    minWidth: 400,
+    maxWidth: 1440,
+    minWidth_FontSize: 12,
+    maxWidth_FontSize: 16,
+    minWidth_LineHeight: 1.5,
+    maxWidth_LineHeight: 1.5,
+    container: "grid",
+    units: "px"
+  },
+  {
+    target: ".slider-container p",
+    minWidth: 400,
+    maxWidth: 1440,
+    minWidth_FontSize: 48,
+    maxWidth_FontSize: 72,
+    minWidth_LineHeight: 1.5,
+    maxWidth_LineHeight: 1.5,
+    container: "grid",
+    units: "px"
+  },
+]);
